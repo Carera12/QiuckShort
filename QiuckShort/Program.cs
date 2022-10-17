b9df1f6f1437b1bd782ff776abd2eb8a68bcbacb
S Chandra <s.chandra.ft21@mail.umy.ac.id>
2022-10-17 17:35:55 +0700

    add if to swap the element at index i whit the element at index j if the greater element is on the left of the element

--- QiuckShort/Program.cs
+++ QiuckShort/Program.cs
@@ -80,6 +80,13 @@ namespace QiuckShort
                     cmp_count--;
                 }
                 cmp_count--;
+
+                if (i < j) // if the greater element is on the left of the element
+                {
+                    //swap the element at index i whit the element at index j
+                    swap(i, j);
+                    mov_count++;
+                }
             }
         }
         static void Main(string[] args)

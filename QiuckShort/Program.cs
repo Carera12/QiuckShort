22c9fb6b366826b0afac9f7c3132d32f8a2ea33f
S Chandra <s.chandra.ft21@mail.umy.ac.id>
2022-10-17 16:56:05 +0700

    add privat int

--- QiuckShort/Program.cs
+++ QiuckShort/Program.cs
@@ -8,6 +8,15 @@ namespace QiuckShort
 {
     class Program
     {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; //number of comparasion
+        private int mov_count = 0; //number of data movements
+
+        //number of elements in array
+        private int n;
+
+
         static void Main(string[] args)
         {
         }

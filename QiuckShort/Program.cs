6fa3239c2dce0e747a90a8c5fbafc67760b9ce6e
S Chandra <s.chandra.ft21@mail.umy.ac.id>
2022-10-17 17:08:35 +0700

    add to get array elements

--- QiuckShort/Program.cs
+++ QiuckShort/Program.cs
@@ -31,6 +31,14 @@ namespace QiuckShort
             Console.WriteLine("\n======================");
             Console.WriteLine("Enter Array Elements");
             Console.WriteLine("\n======================");
+
+            //get array elements
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (1 + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
         }
         static void Main(string[] args)
         {

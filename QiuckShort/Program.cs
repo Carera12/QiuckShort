c5d68de130dd476149761255c6eedce051569cdb
S Chandra <s.chandra.ft21@mail.umy.ac.id>
2022-10-17 17:32:18 +0700

    add while to search for an element less than or equal to pivot

--- QiuckShort/Program.cs
+++ QiuckShort/Program.cs
@@ -72,6 +72,14 @@ namespace QiuckShort
                     cmp_count++;
                 }
                 cmp_count++;
+
+                //search for an element less than or equal to pivot
+                while ((arr[j] > pivot) && (j >= low))
+                {
+                    j--;
+                    cmp_count--;
+                }
+                cmp_count--;
             }
         }
         static void Main(string[] args)
